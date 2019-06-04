be7330e9f4982d1bb288c21058ce75fc574488b9
degski <degski@gmail.com>
2019-06-04 08:33:32 +0300

    input function

--- SimdNet/main.cpp
+++ SimdNet/main.cpp
@@ -161,118 +161,53 @@ struct SnakeSpace {
         }
     }
 
-    // Input (activation) for distance to wall.
-    [[nodiscard]] static float distance_to_wall ( Point const & hp_, ScanDirection const & dir_ ) noexcept {
-        switch ( dir_ ) {
-            case ScanDirection::no: return 1.0f / ( Base - hp_.y + 1 );
-            case ScanDirection::ne: return 1.0f / ( 2 * std::min ( Base - hp_.x, Base - hp_.y ) + 1 );
-            case ScanDirection::ea: return 1.0f / ( Base - hp_.x + 1 );
-            case ScanDirection::se: return 1.0f / ( 2 * std::min ( Base - hp_.x, Base + hp_.y ) + 1 );
-            case ScanDirection::so: return 1.0f / ( Base + hp_.y + 1 );
-            case ScanDirection::sw: return 1.0f / ( 2 * std::min ( Base + hp_.x, Base + hp_.y ) + 1 );
-            case ScanDirection::we: return 1.0f / ( Base + hp_.x + 1 );
-            case ScanDirection::nw: return 1.0f / ( 2 * std::min ( Base + hp_.x, Base - hp_.y ) + 1 );
-        }
-        return NAN;
-    }
-
-    // Input (activation) for distance to point.
-    [[nodiscard]] static float distance_point_to_point ( Point const & p0_, Point const & p1_,
-                                                         ScanDirection const & dir_ ) noexcept {
-        switch ( dir_ ) {
-            case ScanDirection::no: return p0_.x != p1_.x or p0_.y >= p1_.y ? 0.0f : 1.0f / ( p1_.y - p0_.y );
-            case ScanDirection::ne:
-                return p0_.x >= p1_.x or p0_.y >= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                           ? 0.0f
-                           : 1.0f / ( ( p1_.x - p0_.x ) + ( p1_.y - p0_.y ) );
-            case ScanDirection::ea: return p0_.y != p1_.y or p0_.x >= p1_.x ? 0.0f : 1.0f / ( p1_.x - p0_.x );
-            case ScanDirection::se:
-                return p0_.x >= p1_.x or p0_.y <= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                           ? 0.0f
-                           : 1.0f / ( ( p1_.x - p0_.x ) + ( p0_.y - p1_.y ) );
-            case ScanDirection::so: return p0_.x != p1_.x or p0_.y <= p1_.y ? 0.0f : 1.0f / ( p0_.y - p1_.y );
-            case ScanDirection::sw:
-                return p0_.x <= p1_.x or p0_.y <= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                           ? 0.0f
-                           : 1.0f / ( ( p0_.x - p1_.x ) + ( p0_.y - p1_.y ) );
-            case ScanDirection::we: return p0_.y != p1_.y or p0_.x <= p1_.x ? 0.0f : 1.0f / ( p0_.x - p1_.x );
-            case ScanDirection::nw:
-                return p0_.x <= p1_.x or p0_.y >= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                           ? 0.0f
-                           : 1.0f / ( ( p0_.x - p1_.x ) + ( p1_.y - p0_.y ) );
-        }
-        return NAN;
-    }
-
-    static void distances_point_to_point ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
-        out_[ 0 ] = p0_.x != p1_.x or p0_.y >= p1_.y ? 0.0f : 1.0f / ( p1_.y - p0_.y );
-        out_[ 1 ] = p0_.x >= p1_.x or p0_.y >= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                        ? 0.0f
-                        : 1.0f / ( ( p1_.x - p0_.x ) + ( p1_.y - p0_.y ) );
-        out_[ 2 ] = p0_.y != p1_.y or p0_.x >= p1_.x ? 0.0f : 1.0f / ( p1_.x - p0_.x );
-        out_[ 3 ] = p0_.x >= p1_.x or p0_.y <= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                        ? 0.0f
-                        : 1.0f / ( ( p1_.x - p0_.x ) + ( p0_.y - p1_.y ) );
-        out_[ 4 ] = p0_.x != p1_.x or p0_.y <= p1_.y ? 0.0f : 1.0f / ( p0_.y - p1_.y );
-        out_[ 5 ] = p0_.x <= p1_.x or p0_.y <= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                        ? 0.0f
-                        : 1.0f / ( ( p0_.x - p1_.x ) + ( p0_.y - p1_.y ) );
-        out_[ 6 ] = p0_.y != p1_.y or p0_.x <= p1_.x ? 0.0f : 1.0f / ( p0_.x - p1_.x );
-        out_[ 7 ] = p0_.x <= p1_.x or p0_.y >= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                        ? 0.0f
-                        : 1.0f / ( ( p0_.x - p1_.x ) + ( p1_.y - p0_.y ) );
-    }
-
-    static void distances_point_to_point2 ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
-        std::memset ( out_, 0, 8 * sizeof ( float ) );
+    // Manhattan distance activation between points.
+    [[nodiscard]] static std::tuple<int, float> distance_point_to_point ( Point const & p0_, Point const & p1_ ) noexcept {
         Point const s = p0_ - p1_;
-        if ( 0 == s.x ) {
-            if ( s.y > 0 )
-                out_[ 0 ] = 1.0f / s.y;
-            else
-                out_[ 4 ] = 1.0f / s.y;
-        }
+        if ( 0 == s.x )
+            return s.y < 0 ? std::tuple<int, float>{ 0, 1.0f / -s.y } : std::tuple<int, float>{ 4, 1.0f / +s.y };
+        if ( s.x == s.y )
+            return s.x < 0 ? std::tuple<int, float>{ 1, 0.5f / -s.x } : std::tuple<int, float>{ 5, 0.5f / +s.x };
+        if ( 0 == s.y )
+            return s.x < 0 ? std::tuple<int, float>{ 2, 1.0f / -s.x } : std::tuple<int, float>{ 6, 1.0f / +s.x };
+        if ( s.x == -s.y )
+            return s.x < 0 ? std::tuple<int, float>{ 3, 0.5f / -s.x } : std::tuple<int, float>{ 7, 0.5f / +s.x };
+        return std::tuple<int, float>{ 0, 0.0f }; // default north, but set to zero, which avoids checking.
     }
 
-    [[nodiscard]] float distance_to_body ( ScanDirection const & dir_ ) const noexcept {
-        Point const head = m_snake_body.front ( );
-        float distance   = 0.0f;
-        auto const end   = std::end ( m_snake_body );
-        auto it          = std::next ( std::begin ( m_snake_body ) );
-        while ( it != end ) {
-            float const d = distance_point_to_point ( head, *it, dir_ );
-            if ( d > distance )
-                distance = d;
-            ++it;
-        }
-        return distance;
+    // Input (activation) for distance to wall.
+    void distances_to_wall ( float * dist_ ) const noexcept {
+        Point const & head = m_snake_body.front ( );
+        dist_[ 0 ]         = 1.0f / ( Base - head.y + 1 );
+        dist_[ 1 ]         = 1.0f / ( 2 * std::min ( Base - head.x, Base - head.y ) + 1 );
+        dist_[ 2 ]         = 1.0f / ( Base - head.x + 1 );
+        dist_[ 3 ]         = 1.0f / ( 2 * std::min ( Base - head.x, Base + head.y ) + 1 );
+        dist_[ 4 ]         = 1.0f / ( Base + head.y + 1 );
+        dist_[ 5 ]         = 1.0f / ( 2 * std::min ( Base + head.x, Base + head.y ) + 1 );
+        dist_[ 6 ]         = 1.0f / ( Base + head.x + 1 );
+        dist_[ 7 ]         = 1.0f / ( 2 * std::min ( Base + head.x, Base - head.y ) + 1 );
+    }
+
+    void distances_to_food ( float * dist_ ) const noexcept {
+        auto const [ dir, val ] = distance_point_to_point ( m_snake_body.front ( ), m_food );
+        dist_[ dir ]            = val;
     }
 
     void distances_to_body ( float * dist_ ) const noexcept {
         Point const & head = m_snake_body.front ( );
         auto const end     = std::end ( m_snake_body );
-        auto it            = std::next ( std::begin ( m_snake_body ) ); // This assumes the length of the snake is at least 2.
-        float d[ 8 ];
-        distances_point_to_point ( dist_, head, *it );
+        auto it            = std::begin ( m_snake_body ); // This assumes the length of the snake is at least 2.
         while ( ++it != end ) {
-            distances_point_to_point ( d, head, *it );
-            if ( d[ 0 ] > dist_[ 0 ] )
-                dist_[ 0 ] = d[ 0 ];
-            if ( d[ 1 ] > dist_[ 1 ] )
-                dist_[ 1 ] = d[ 1 ];
-            if ( d[ 2 ] > dist_[ 2 ] )
-                dist_[ 2 ] = d[ 2 ];
-            if ( d[ 3 ] > dist_[ 3 ] )
-                dist_[ 3 ] = d[ 3 ];
-            if ( d[ 4 ] > dist_[ 4 ] )
-                dist_[ 4 ] = d[ 4 ];
-            if ( d[ 5 ] > dist_[ 5 ] )
-                dist_[ 5 ] = d[ 5 ];
-            if ( d[ 6 ] > dist_[ 6 ] )
-                dist_[ 6 ] = d[ 6 ];
-            if ( d[ 7 ] > dist_[ 7 ] )
-                dist_[ 7 ] = d[ 7 ];
+            auto const [ dir, val ] = distance_point_to_point ( head, *it );
+            if ( val > dist_[ dir ] )
+                dist_[ dir ] = val;
         }
+    }
+
+    void distances ( std::array<float, 24> & d_ ) const noexcept {
+        distances_to_wall ( d_.data ( ) + 0 );
+        distances_to_food ( d_.data ( ) + 8 );
+        distances_to_body ( d_.data ( ) + 16 );
     }
 
     void print ( ) const noexcept {
@@ -300,24 +235,68 @@ struct SnakeSpace {
     Point m_food;
 };
 
-void distances_point_to_point ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
-    out_[ 0 ] = p0_.x != p1_.x or p0_.y >= p1_.y ? 0.0f : 1.0f / ( p1_.y - p0_.y );
-    out_[ 1 ] = p0_.x >= p1_.x or p0_.y >= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                    ? 0.0f
-                    : 1.0f / ( ( p1_.x - p0_.x ) + ( p1_.y - p0_.y ) );
-    out_[ 2 ] = p0_.y != p1_.y or p0_.x >= p1_.x ? 0.0f : 1.0f / ( p1_.x - p0_.x );
-    out_[ 3 ] = p0_.x >= p1_.x or p0_.y <= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                    ? 0.0f
-                    : 1.0f / ( ( p1_.x - p0_.x ) + ( p0_.y - p1_.y ) );
-    out_[ 4 ] = p0_.x != p1_.x or p0_.y <= p1_.y ? 0.0f : 1.0f / ( p0_.y - p1_.y );
-    out_[ 5 ] = p0_.x <= p1_.x or p0_.y <= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
-                    ? 0.0f
-                    : 1.0f / ( ( p0_.x - p1_.x ) + ( p0_.y - p1_.y ) );
-    out_[ 6 ] = p0_.y != p1_.y or p0_.x <= p1_.x ? 0.0f : 1.0f / ( p0_.x - p1_.x );
-    out_[ 7 ] = p0_.x <= p1_.x or p0_.y >= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
-                    ? 0.0f
-                    : 1.0f / ( ( p0_.x - p1_.x ) + ( p1_.y - p0_.y ) );
+int main ( ) {
+
+    SnakeSpace<17> ss;
+
+    ss.print ( );
+
+    std::array<float, 24> d{};
+
+    ss.distances ( d );
+
+    for ( auto f : d )
+        std::cout << f << ' ';
+    std::cout << nl;
+
+    /*
+
+    // ss.run ( );
+
+
+    constexpr int in = 128;
+
+    FullyConnectedNeuralNetwork<in, 150, 1> nn;
+
+    typename FullyConnectedNeuralNetwork<in, 150, 1>::ibo_type ibo;
+
+    float x = 0.0;
+
+    plf::nanotimer t;
+
+    t.start ( );
+
+    for ( int i = 0; i < 100'000; ++i ) {
+        for ( int j = 0; j < in; ++j )
+            ibo[ j ] = std::uniform_real_distribution<float> ( -0.25f, 0.25f ) ( Rng::gen ( ) );
+        auto out = nn.feed_forward ( ibo.data ( ) );
+        x += out[ 0 ];
+    }
+
+    std::cout << ( std::uint64_t ) t.get_elapsed_ms ( ) << nl;
+
+    std::cout << x << nl;
+    */
+    return EXIT_SUCCESS;
 }
+
+/*
+
+
+    // Input (activation) for distance to wall.
+    [[nodiscard]] static float distance_to_wall ( Point const & hp_, ScanDirection const & dir_ ) noexcept {
+        switch ( dir_ ) {
+            case ScanDirection::no: return 1.0f / ( Base - hp_.y + 1 );
+            case ScanDirection::ne: return 1.0f / ( 2 * std::min ( Base - hp_.x, Base - hp_.y ) + 1 );
+            case ScanDirection::ea: return 1.0f / ( Base - hp_.x + 1 );
+            case ScanDirection::se: return 1.0f / ( 2 * std::min ( Base - hp_.x, Base + hp_.y ) + 1 );
+            case ScanDirection::so: return 1.0f / ( Base + hp_.y + 1 );
+            case ScanDirection::sw: return 1.0f / ( 2 * std::min ( Base + hp_.x, Base + hp_.y ) + 1 );
+            case ScanDirection::we: return 1.0f / ( Base + hp_.x + 1 );
+            case ScanDirection::nw: return 1.0f / ( 2 * std::min ( Base + hp_.x, Base - hp_.y ) + 1 );
+        }
+        return NAN;
+    }
 
 void dp2p ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
     Point const s = p0_ - p1_;
@@ -351,102 +330,52 @@ void dp2p ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
     }
 }
 
-int main ( ) {
-
-    Point b{ 8, -7 };
-    Point h{ -2, 1 };
-
-    float dists1[ 8 ]{};
-
-    dp2p ( dists1, b, h );
-
-    for ( auto f : dists1 )
-        std::cout << f << ' ';
-    std::cout << nl;
-
-    float dists2[ 8 ]{};
-
-    distances_point_to_point ( dists2, b, h );
-
-    for ( auto f : dists2 )
-        std::cout << f << ' ';
-    std::cout << nl;
-
-    /*
-
-    SnakeSpace<17> ss;
-
-    Point f{ -8, -8 };
-    Point p{ 6, 6 };
-
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::no ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::ne ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::ea ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::nw ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::so ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::sw ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::we ) << nl;
-    std::cout << SnakeSpace<17>::distance_point_to_point ( p, f, SnakeSpace<17>::ScanDirection::se ) << nl;
-
-    float dists[ 8 ];
-
-    SnakeSpace<17>::distances_point_to_point ( dists, p, f );
-
-    for ( auto f : dists )
-        std::cout << f << ' ';
-    std::cout << nl;
-
-    ss.print ( );
-
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::no ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::ne ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::ea ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::nw ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::so ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::sw ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::we ) << nl;
-    std::cout << ss.distance_to_body ( SnakeSpace<17>::ScanDirection::se ) << nl;
-
-    float dists2[ 8 ];
-
-    ss.distances_to_body ( dists2 );
-
-    for ( auto f : dists2 )
-        std::cout << f << ' ';
-    std::cout << nl;
-
-    ss.print ( );
-
-    // ss.run ( );
-
-
-    constexpr int in = 128;
-
-    FullyConnectedNeuralNetwork<in, 150, 1> nn;
-
-    typename FullyConnectedNeuralNetwork<in, 150, 1>::ibo_type ibo;
-
-    float x = 0.0;
-
-    plf::nanotimer t;
-
-    t.start ( );
-
-    for ( int i = 0; i < 100'000; ++i ) {
-        for ( int j = 0; j < in; ++j )
-            ibo[ j ] = std::uniform_real_distribution<float> ( -0.25f, 0.25f ) ( Rng::gen ( ) );
-        auto out = nn.feed_forward ( ibo.data ( ) );
-        x += out[ 0 ];
+    // Input (activation) for distance to point.
+    [[nodiscard]] static float distance_point_to_point ( Point const & p0_, Point const & p1_,
+                                                         ScanDirection const & dir_ ) noexcept {
+        switch ( dir_ ) {
+            case ScanDirection::no: return p0_.x != p1_.x or p0_.y >= p1_.y ? 0.0f : 1.0f / ( p1_.y - p0_.y );
+            case ScanDirection::ne:
+                return p0_.x >= p1_.x or p0_.y >= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
+                           ? 0.0f
+                           : 1.0f / ( ( p1_.x - p0_.x ) + ( p1_.y - p0_.y ) );
+            case ScanDirection::ea: return p0_.y != p1_.y or p0_.x >= p1_.x ? 0.0f : 1.0f / ( p1_.x - p0_.x );
+            case ScanDirection::se:
+                return p0_.x >= p1_.x or p0_.y <= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
+                           ? 0.0f
+                           : 1.0f / ( ( p1_.x - p0_.x ) + ( p0_.y - p1_.y ) );
+            case ScanDirection::so: return p0_.x != p1_.x or p0_.y <= p1_.y ? 0.0f : 1.0f / ( p0_.y - p1_.y );
+            case ScanDirection::sw:
+                return p0_.x <= p1_.x or p0_.y <= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
+                           ? 0.0f
+                           : 1.0f / ( ( p0_.x - p1_.x ) + ( p0_.y - p1_.y ) );
+            case ScanDirection::we: return p0_.y != p1_.y or p0_.x <= p1_.x ? 0.0f : 1.0f / ( p0_.x - p1_.x );
+            case ScanDirection::nw:
+                return p0_.x <= p1_.x or p0_.y >= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
+                           ? 0.0f
+                           : 1.0f / ( ( p0_.x - p1_.x ) + ( p1_.y - p0_.y ) );
+        }
+        return NAN;
     }
 
-    std::cout << ( std::uint64_t ) t.get_elapsed_ms ( ) << nl;
-
-    std::cout << x << nl;
-    */
-    return EXIT_SUCCESS;
+void distances_point_to_point ( float * out_, Point const & p0_, Point const & p1_ ) noexcept {
+    out_[ 0 ] = p0_.x != p1_.x or p0_.y >= p1_.y ? 0.0f : 1.0f / ( p1_.y - p0_.y );
+    out_[ 1 ] = p0_.x >= p1_.x or p0_.y >= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
+                    ? 0.0f
+                    : 1.0f / ( ( p1_.x - p0_.x ) + ( p1_.y - p0_.y ) );
+    out_[ 2 ] = p0_.y != p1_.y or p0_.x >= p1_.x ? 0.0f : 1.0f / ( p1_.x - p0_.x );
+    out_[ 3 ] = p0_.x >= p1_.x or p0_.y <= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
+                    ? 0.0f
+                    : 1.0f / ( ( p1_.x - p0_.x ) + ( p0_.y - p1_.y ) );
+    out_[ 4 ] = p0_.x != p1_.x or p0_.y <= p1_.y ? 0.0f : 1.0f / ( p0_.y - p1_.y );
+    out_[ 5 ] = p0_.x <= p1_.x or p0_.y <= p1_.y or ( ( p0_.x - p0_.y ) != ( p1_.x - p1_.y ) )
+                    ? 0.0f
+                    : 1.0f / ( ( p0_.x - p1_.x ) + ( p0_.y - p1_.y ) );
+    out_[ 6 ] = p0_.y != p1_.y or p0_.x <= p1_.x ? 0.0f : 1.0f / ( p0_.x - p1_.x );
+    out_[ 7 ] = p0_.x <= p1_.x or p0_.y >= p1_.y or ( ( p0_.x + p0_.y ) != ( p1_.x + p1_.y ) )
+                    ? 0.0f
+                    : 1.0f / ( ( p0_.x - p1_.x ) + ( p1_.y - p0_.y ) );
 }
-
-/*
 
 
 template<int NumInput, int NumNeurons, int NumOutput>
